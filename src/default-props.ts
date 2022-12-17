ddb2f3aff73a407358d79b14324d198c50776d89
Eduardo García Maleta <egmaleta.secondary@proton.me>
2022-12-17 18:17:32 -0500

    feat: add icon default props

---
+++ src/default-props.ts
@@ -0,0 +1,9 @@
+import { IconProps } from "./types";
+
+export default {
+  size: 24,
+  color: 'currentColor',
+  strokeWidth: 2,
+  strokeLinecap: 'round',
+  strokeLinejoin: 'round'
+} as Required<IconProps>;
