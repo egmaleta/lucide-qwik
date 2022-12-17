dfc0b4d7c0c42e5d7212a9bbb82054666de8374f
Eduardo García Maleta <egmaleta.secondary@proton.me>
2022-12-17 18:18:08 -0500

    feat: add base icon implementation

---
+++ src/components/base-icon.tsx
@@ -0,0 +1,25 @@
+import { component$, Slot } from "@builder.io/qwik";
+
+import { BaseIconProps } from "../types";
+import fallbackProps from "../default-props";
+
+export default component$(({
+  name,
+  size = fallbackProps.size,
+  color = fallbackProps.color,
+  strokeWidth = fallbackProps.strokeWidth,
+  strokeLinecap = fallbackProps.strokeLinecap,
+  strokeLinejoin = fallbackProps.strokeLinejoin,
+  ...restProps
+}: BaseIconProps) => {
+  return <svg
+    {...restProps}
+    xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none'
+    width={size} height={size}
+    stroke={color} stroke-width={strokeWidth}
+    stroke-linecap={strokeLinecap} stroke-linejoin={strokeLinejoin}
+    class={`lucide lucide-${name} ${restProps.class ?? ''}`}
+  >
+    <Slot/>
+  </svg>;
+});
